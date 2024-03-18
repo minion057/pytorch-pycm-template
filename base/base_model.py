0d934ef564cdc76619a7630c1f9e5dd3c2fcb0d1
minion057 <getit3981@gmail.com>
2024-03-18 17:29:15 +0900

    feat: Calculate layer output size.
    Calculate the size of the output from convolutional layers and pooling layers.

--- base/base_model.py
+++ base/base_model.py
@@ -23,3 +23,23 @@ class BaseModel(nn.Module):
         model_parameters = filter(lambda p: p.requires_grad, self.parameters())
         params = sum([np.prod(p.size()) for p in model_parameters])
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
+
+    def conv_output_size(self, IS, KS, PS:int=0, ST:int=1):
+        """
+        Calculate the output size of convolution layer. ((H-FH+2P)/strid+1)//2
+        
+        :param IS:  Input   size (height or width)
+        :param KS:  Kernel  size
+        :param PS:  Padding size
+        :param ST:  Stride  size
+        """
+        return (IS-KS+PS*2)//ST+1
+        
+    def pooling_output_size(self, IS, PS):
+        """
+        Calculate the output size of pooling layer. H//ps
+        
+        :param IS: Input   size (height or width)
+        :param PS: Pooling size
+        """
+        return IS//PS
