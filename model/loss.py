50bcbba83407ed256820eb0669460755901610c4
minion057 <getit3981@gmail.com>
2024-03-18 17:43:36 +0900

    feat: Added loss function.
    Support for Cross-Entropy (CE) and Binary Cross-Entropy (BCE) loss.

--- model/loss.py
+++ model/loss.py
@@ -3,3 +3,10 @@ import torch.nn.functional as F
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
+
+def ce_loss(output, target): # cross_entropy_loss
+    return F.cross_entropy(output, target)
+
+def bce_loss(output, target): # binary_cross_entropy
+    # return F.binary_cross_entropy(torch.sigmoid(input), target)
+    return F.binary_cross_entropy_with_logits(output, target)
