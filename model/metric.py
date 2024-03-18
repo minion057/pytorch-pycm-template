6ac5b3a452310219a7bca554a88d21c14e342ebe
minion057 <getit3981@gmail.com>
2024-03-18 17:46:36 +0900

    style: Metrics using pycm.

--- model/metric.py
+++ model/metric.py
@@ -1,20 +1,29 @@
-import torch
+from pycm import ConfusionMatrix as pycmCM
 
 
-def accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
+""" Accuracy """
+def ACC(confusion_obj:pycmCM, classes=None, specific_class_idx=None):
+    return confusion_obj.Overall_ACC if specific_class_idx is None else confusion_obj.ACC[specific_class_idx]
+def ACC_class(confusion_obj:pycmCM, classes=None):
+    # return confusion_obj.ACC
+    return {classes[class_idx]:score for class_idx, score in enumerate(confusion_obj.ACC.values())}
 
+""" Sensitivity, hit rate, recall, or true positive rate """
+def TPR(confusion_obj:pycmCM, classes=None, specific_class_idx=None):
+    # 전체 클래스의 TPR 평균 반환
+    if specific_class_idx is None: return confusion_obj.TPR_Macro if confusion_obj.TPR_Macro != 'None' else 0.
+    # 특정(i.g,암) 클래스의 TPR 평균 반환
+    return confusion_obj.TPR[specific_class_idx]
+def TPR_class(confusion_obj:pycmCM, classes=None):
+    # return confusion_obj.TPR
+    return {classes[class_idx]:score for class_idx, score in enumerate(confusion_obj.TPR.values())}
 
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
+""" Specificity or true negative rate """
+def TNR(confusion_obj:pycmCM, classes=None, specific_class_idx=None):
+    # 전체 클래스의 TNR 평균 반환
+    if specific_class_idx is None: return confusion_obj.TNR_Macro if confusion_obj.TNR_Macro != 'None' else 0.
+    # 특정(i.g,정상) 클래스의 TNR 평균 
+    return confusion_obj.TNR[specific_class_idx]
+def TNR_class(confusion_obj:pycmCM, classes=None):
+    # return confusion_obj.TNR
+    return {classes[class_idx]:score for class_idx, score in enumerate(confusion_obj.TNR.values())}
