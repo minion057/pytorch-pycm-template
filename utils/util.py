c01f5d60d023eb9f330461e4fffc4e1ffc85db0e
minion057 <getit3981@gmail.com>
2024-07-10 16:19:04 +0900

    Feature: Added the ability to change dictionary values to a list when saving metric results to a JSON file.

--- utils/util.py
+++ utils/util.py
@@ -20,13 +20,33 @@ def read_json(fname):
 
 def write_json(content, fname):
     fname = Path(fname)
+    content = convert_keys_to_string(content)
     with fname.open('wt') as handle:
         json.dump(content, handle, indent=4, sort_keys=False)
 
 def write_dict2json(content, file_path):
+    content = convert_keys_to_string(content)
     with open(file_path, 'w', encoding='utf-8') as handle:
         json.dump(content, handle, ensure_ascii=False, indent='\t')
+
+def convert_keys_to_string(d):
+    if not isinstance(d, dict): return d
+
+    new_dict = {}
+    for key, value in d.items():
+        # Convert key to string
+        new_key = str(key)
         
+        # Recursively apply to nested dictionaries
+        if isinstance(value, dict): 
+            new_value = convert_keys_to_string(value)
+        elif isinstance(value, list): 
+            new_value = [convert_keys_to_string(item) if isinstance(item, dict) else item for item in value]
+        else: new_value = value
+        
+        new_dict[new_key] = new_value
+    return new_dict
+    
 def inf_loop(data_loader):
     ''' wrapper function for endless data loader. '''
     for loader in repeat(data_loader):
@@ -76,10 +96,11 @@ def tb_projector_resize(data, label_img, features):
 
 def check_onehot_label(item, classes):
     item_class = np.unique(np.array(item), return_counts=True)[0]
-    if type(item) == int: return False
-    elif len(item_class) != len(classes): return False #print('class num')
-    elif 0 not in item_class and 1 not in item_class: return False #print(item_class)
-    else: return True
+    if type(item) in [list, np.ndarray]:      
+        if all([0, 1] == item_class): return True
+        if all(i in classes for i in item_class): return False
+    else: return False
+    return True
 
 def onehot_encoding(label, classes):
     if type(classes) == np.ndarray: classes = classes.tolist() # for FutureWarning by numpy
@@ -97,9 +118,17 @@ def onehot_encoding(label, classes):
 
 def integer_encoding(label, classes): #  by index of classes
     if type(classes) == np.ndarray: classes = classes.tolist() # for FutureWarning by numpy
-    sorted_classes = deepcopy(classes); sorted_classes.sort()
+    label_classes = np.unique(label)
     item = label[0]
-    if check_onehot_label(item, classes) and type(item) in [list, np.ndarray]: label2label = np.argmax(label, axis=1)
-    elif all(np.unique(label) != sorted_classes) or sorted_classes[0] != 0: label2label = [classes.index(a) for a in label]
-    else: label2label = np.array(label)
+    
+    if check_onehot_label(item, classes): # Is onehot
+        label2label = np.argmax(np.array(label), axis=1) 
+    elif np.array(classes).dtype == label_classes.dtype:
+        # Class and label are the same type as label, so get the index.
+        label2label = [classes.index(a) for a in label]
+    elif label_classes.dtype in [np.int32, np.int64] and all(label_classes == np.array(list(range(len(label_classes))))):
+        # Label is a number and has elements from 0 to the length of label_classes, so use it as an index.
+        # Use np.int32, np.int64 instead of np.integer for future compatibility.
+        label2label = np.array(label)
+    else: raise ValueError(f'Unable to convert label to integer type.\nCurrent classes: {classes}, items in label: {label_classes}.')
     return label2label
