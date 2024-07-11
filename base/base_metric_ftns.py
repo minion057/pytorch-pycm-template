930065894fe80f7fe29c83dc0ce056f7702356a3
minion057 <getit3981@gmail.com>
2024-07-11 13:32:46 +0900

    Fix: Modified to use the class applied to the metric when retrieving the metric score for a specific class.

--- base/base_metric_ftns.py
+++ base/base_metric_ftns.py
@@ -9,15 +9,16 @@ def base_metric(ftns_name, confusion_obj:pycmCM, positive_class_idx=None, averag
             metric_name = f'Overall_{ftns_name}'
         elif ftns_name in ['ACC', 'PPV', 'NPV', 'TPR', 'TNR', 'FPR', 'FNR', 'F1']:
             if average_type not in ['Macro', 'Micro']: raise TypeError('The metric you set up can be averaged macro or micro.')
-            if average_type == 'Micro' and ftns_name == 'ACC': raise TypeError('정확도에서 micro 방식은 지원되지 않습니다.')
+            if average_type == 'Micro' and ftns_name == 'ACC': raise TypeError('The micro method is not supported for accuracy.')
             metric_name = f'{ftns_name}_{average_type}'
         else: metric_name = ftns_name
         if metric_name not in dir(confusion_obj): raise ValueError(f'This metric ({ftns_name}) is not supported by the pycm library.')
         score = eval(f'use_confusion_obj.{metric_name}')
     else:
         if ftns_name not in dir(confusion_obj): raise ValueError(f'This metric ({ftns_name}) is not supported by the pycm library.')
-        classes = list(use_confusion_obj.classes)
-        score = eval(f'use_confusion_obj.{ftns_name}[classes[positive_class_idx]]')
+        # classes = list(use_confusion_obj.classes)
+        score = eval(f'use_confusion_obj.{ftns_name}')
+        score = score[list(score.keys())[positive_class_idx]]
     return score if score != 'None' else 0.
     
 def base_class_metric(ftns_name, confusion_obj:pycmCM, classes=None, **kwargs):
