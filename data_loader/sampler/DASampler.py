f91afa7e429e0694ba73ac0243d284f885bf4586
minion057 <getit3981@gmail.com>
2024-09-11 15:57:56 +0900

    Featrue: Add code to convert data to a tensor for DA classes and restore it to its original state.

--- data_loader/sampler/DASampler.py
+++ data_loader/sampler/DASampler.py
@@ -6,14 +6,14 @@ from utils import check_and_import_library, integer_encoding
 
 class DASampler(BaseSampler):   
     def _check_kwargs(self):
-        if self.sampler_type.lower() in ['mixup', 'cutmix']:
-            raise ValueError(f'DASampler does not support {self.sampler_type}. This can be done by utilizing a batch sampler, or by applying prob as a `None` of the DA object.')
+        if self.sampler_name.lower() in ['mixup', 'cutmix']:
+            raise ValueError(f'DASampler does not support {self.sampler_name}. This can be done by utilizing a batch sampler, or by applying prob as a `None` of the DA object.')
         
         required_kwargs = {'use_balanced_sampler':True, 'sampling_strategy':'auto', 'sampling_multiplier':0.}
         if self.kwargs == {}: self.kwargs = deepcopy(required_kwargs)
         for required_param in required_kwargs.keys():
             if required_param not in self.kwargs.keys(): 
-                raise ValueError(f'The required parameter ({required_param}) is not set. But got {self.kwargs}.')
+                raise ValueError(f'The required parameter ({required_param}) is not set.')
             if required_param == 'sampling_multiplier':
                 if not isinstance(self.kwargs[required_param], (int, float, list)):
                     raise TypeError(f'The required parameter ({required_param}) must be int, float, list type. But got {type(self.kwargs[required_param])}.')
@@ -24,27 +24,36 @@ class DASampler(BaseSampler):
                         error_message = f'The length of the required parameter ({required_param}) must be equal to the length of the classes.'
                         error_message += f'\nBut got {len(self.kwargs[required_param])} and {len(self.classes)}.'
                         raise ValueError(error_message)
-            elif not isinstance(self.kwargs[required_param], type(required_param)):
-                raise TypeError(f'The required parameter ({required_param}) must be {type(required_param)} type. But got {type(self.kwargs[required_param])}.')
+            elif not isinstance(self.kwargs[required_param], type(required_kwargs[required_param])):
+                raise TypeError(f'The required parameter ({required_param}) must be {type(required_kwargs[required_param])} type. But got {type(self.kwargs[required_param])}.')
                
     def _run(self):
-        targets2integer = self._convert_targets2integer(self.targets)
+        data2tensor, targets2integer = self._convert_data2tensor(), self._convert_targets2integer()
         target_indices_per_class_index = {class_index:np.where(targets2integer==class_index)[0] for class_index in range(len(self.classes))}
         class_sampling_counts = self._calculate_sampling_counts(self.kwargs['use_balanced_sampler'], targets2integer) # {class_index: num_samples_to_generate}
         sampler = self._get_a_sampler()
-        
-        sampled_data, sampled_targets = deepcopy(self.data), deepcopy(targets2integer)
+
+        sampled_data, sampled_targets = data2tensor.detach().cpu().numpy(), deepcopy(targets2integer)
+        self.da_indices = {}
         for class_index, target_indices in target_indices_per_class_index.items():
+            if class_index not in class_sampling_counts: continue
+            
             num_samples_to_generate = class_sampling_counts[class_index]
             sampled_indices = np.random.choice(
                 target_indices, 
                 size=num_samples_to_generate, 
                 replace=(num_samples_to_generate > len(target_indices)) 
             )
-            sampled_data = np.concatenate((sampled_data, sampler._run(torch.Tensor(self.data[sampled_indices])).detach().cpu().numpy()), axis=0)
-            sampled_targets = np.concatenate((sampled_targets, targets2integer[sampled_indices]), axis=0)
+            self.da_indices[class_index] = deepcopy(sampled_indices)
             
-        self.data_source.data = sampled_data
+            numpy_data = []
+            for sampled_index in sampled_indices: 
+                numpy_data.append(sampler._run(data2tensor[sampled_index]).detach().cpu().numpy())
+            
+            sampled_data = np.concatenate((sampled_data, np.array(numpy_data)), axis=0)
+            sampled_targets = np.concatenate((sampled_targets, targets2integer[sampled_indices]), axis=0)
+        
+        self.data_source.data = self._revert_data(sampled_data)
         self.data_source.targets = self._revert_targets(sampled_targets)
       
     def _calculate_sampling_counts(self, using_imblearn:bool, integer_encoded_targets=None): 
@@ -61,9 +70,11 @@ class DASampler(BaseSampler):
             'auto': equivalent to 'not majority'.
         '''
         if using_imblearn: 
-            if integer_encoded_targets is None: raise ValueError('The integer_encoded_targets must be set via the `_convert_targets2integer` function.')
+            if integer_encoded_targets is None: 
+                raise ValueError('The integer_encoded_targets must be set via the `_convert_targets2integer` function.')
             strategy_module = check_and_import_library(f'imblearn.utils')
-            if not hasattr(module, 'check_sampling_strategy'): raise ValueError('This sampler requires imblearn\'s check_sampling_strategy to work.')
+            if not hasattr(strategy_module, 'check_sampling_strategy'): 
+                raise ValueError('This sampler requires imblearn\'s check_sampling_strategy to work.')
             counts_module =getattr(strategy_module, 'check_sampling_strategy')
             class_sampling_counts = counts_module(self.kwargs['sampling_strategy'], integer_encoded_targets, self.sampler_type)
             max_class_cnt = max(self.target_counts)
@@ -83,7 +94,22 @@ class DASampler(BaseSampler):
     def _get_a_sampler(self):
         module = check_and_import_library(f'data_loader.data_augmentation')
         if not hasattr(module, self.sampler_name): raise ValueError('The sampler_name does not exist.')
-        sampler = getattr(module, self.sampler_type)(**self.sampler_kwargs)
+        sampler = getattr(module, self.sampler_name)(**self.sampler_kwargs)
         if not hasattr(sampler, '_run'): raise ValueError('The function (`_run`) that performs DA does not exist.')
         return sampler
     
+    def _convert_data2tensor(self):
+        converted_data = []
+        for item in deepcopy(self.data):
+            # RGB channel
+            if len(item.shape) == 2:
+                item = np.tile(np.expand_dims(item, axis=0), [3, 1, 1])
+            elif len(item.shape) != 3: 
+                raise ValueError('The data must have 2 or 3 dimensions.')
+            else:
+                item = np.moveaxis(item, -1, 0)
+            converted_data.append(item)
+        return torch.from_numpy(np.array(converted_data))
+    
+    def _revert_data(self, converted_data):
+        return converted_data[:, 0, :, :]
