e56f2659e0cecacd1a4d9696089a74d126ffb88e
minion057 <getit3981@gmail.com>
2024-09-12 10:22:21 +0900

    Chore

--- data_loader/data_sampling.py
+++ data_loader/data_sampling.py
@@ -1,4 +1,2 @@
-# from .sampler.undersampling import *
-# from .sampler.oversampling import *
 from .sampler.ImbalancedlearnSampler import ImbalancedlearnSampler
 from .sampler.DASampler import DASampler
