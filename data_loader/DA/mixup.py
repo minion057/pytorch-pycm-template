Fix: Reorder loss calculation in mixup to prevent errors

data_loader/DA/mixup.py
@@ -73,9 +73,9 @@ class MixUp(BaseHook):
     
     def loss(self, loss_ftns, output, target, logit):
         random_index, lam = self.rand_index(), self.lam()
-        if random_index is None: return loss
-        if len(random_index) != len(target): raise ValueError('Target and the number of shuffled indexes do not match.')
         basic_loss  = loss_ftns(output[:len(target)], target, logit)
+        if random_index is None: return {'loss':basic_loss, 'target':target}
+        if len(random_index) != len(target): raise ValueError('Target and the number of shuffled indexes do not match.')
         random_loss = loss_ftns(output[len(target):] if self.prob is None else output, target[random_index], logit)
         loss = basic_loss*lam + random_loss*(1.-lam)
         return {'loss':loss, 'target':torch.cat((target, target[random_index]), 0) if self.prob is None else target}