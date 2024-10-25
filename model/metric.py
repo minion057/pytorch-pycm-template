8a0c5404c5d799e60304bc01cd2006917200399a
minion057 <getit3981@gmail.com>
2024-10-25 13:20:09 +0900

    Feat: Differentiate AUC calculation methods using PyCM and ROC-based scores

--- model/metric.py
+++ model/metric.py
@@ -1,15 +1,7 @@
-import traceback
-try:
-    from .metric_custom import *
-except:
-    print('Please check if "model/metric_custom.py" exists, and if it does, add its path to sys.path.')
-    print('If necessary, please create and write the file. If not necessary, please comment out this section.')
-    print(traceback.format_exc())
-
 import numpy as np
 from pycm import ConfusionMatrix as pycmCM
-from pycm import ROCCurve
 from base import BaseMetricFtns
+from utils import check_and_import_library
 
 """ 
 All metrics use the one-vs-rest strategy. 
@@ -74,28 +66,61 @@ Warring: The AUC calculated using the ROC curve and the AUC calculated using the
 ROC CURVE calculates the threshold in detail. 
 AUC is calculated in a simpler way, which may result in a different value than the AUC obtained through ROC CURVE.
 """
-def AUC_OvR(confusion_obj:pycmCM, classes=None, positive_class_idx=None, method:str='basic'):
-    if method not in ['basic', 'roc']: raise ValueError('The method can only be "basic" or "roc".')
-    if positive_class_idx is None: raise ValueError('AUC requires positive_class_idx.')
-    return list(AUC_OvR_class(confusion_obj, classes, [positive_class_idx], method).values())[0]
-def AUC_OvR_class(confusion_obj:pycmCM, classes=None, positive_class_indices=None, method:str='basic'):
-    if method not in ['basic', 'roc']: raise ValueError('The method can only be "basic" or "roc".')
-    
+def AUC(confusion_obj:pycmCM, classes=None, positive_class_idx=None):
     # Instantiate objects and verify class correctness
     ftns = BaseMetricFtns(confusion_obj=confusion_obj, classes=classes)
-    ftns_name = 'AUC'
+    return ftns.single_class_metric('AUC', positive_class_idx)
+def AUC_class(confusion_obj:pycmCM, classes=None, positive_class_idx=None):
+    # Instantiate objects and verify class correctness
+    ftns = BaseMetricFtns(confusion_obj=confusion_obj, classes=classes)
+    return ftns.multi_class_metric('AUC', positive_class_indices)
+
+def AUC_OvR(confusion_obj:pycmCM, classes, positive_class_idx=None):
+    if positive_class_idx is None: raise ValueError('AUC requires positive_class_idx.')
+    return list(AUC_OvR_class(confusion_obj, classes, [positive_class_idx]).values())[0]
+def AUC_OvR_class(confusion_obj:pycmCM, classes, positive_class_indices=None):
+    # Instantiate objects and verify class correctness
+    ftns = BaseMetricFtns(confusion_obj=confusion_obj, classes=classes)
     
-    # basic version
-    if method.lower() == 'basic': return ftns.multi_class_metric(ftns_name, positive_class_indices)
-    # roc version
+    if classes is None: raise ValueError('CLASSES is required to be entered in order to calculate with the ROC version.')
+    ftns_name, metrics_module = 'ROC_OvR', check_and_import_library('model.plottable_metrics')
+    if ftns_name not in dir(metrics_module): raise ValueError(f'Warring: {ftns_name} is not in the model.metric library.')
+    use_ftns = getattr(metrics_module, ftns_name)
+    
+    target_classes = list(np.unique(confusion_obj.actual_vector))
+    if len(target_classes) == 1: return {c:0. for c in classes} # AUC area not available
+    if not all(class_item in list(target_classes) for class_item in list(classes)):
+        print(f"Warning: Classes in current label ({target_classes}) do not match classes in confusion object ({classes})."
+              +"Please ensure that the classes are consistent.")
+    
     if confusion_obj.prob_vector is None: raise ValueError('No value for prob vector.')
     if isinstance(confusion_obj.prob_vector, dict):
         confusion_obj.prob_vector = confusion_obj.prob_vector['all_probs']
+        
+    roc_dict, _ = use_ftns(labels=confusion_obj.actual_vector, probs=confusion_obj.prob_vector, classes=classes, return_result=True)
+    return ftns._format_metric_for_multi_score('AUC', {t_c:auc for t_c, auc in zip(classes, roc_dict['auc'])}, positive_class_indices)
+
+def AUC_OvO(confusion_obj:pycmCM, classes, positive_class_idx=None):
+    if positive_class_idx is None: raise ValueError('AUC requires positive_class_idx.')
+    return list(AUC_OvO_class(confusion_obj, classes, [positive_class_idx]).values())[0]
+def AUC_OvO_class(confusion_obj:pycmCM, classes, positive_class_indices=None):
+    # Instantiate objects and verify class correctness
+    ftns = BaseMetricFtns(confusion_obj=confusion_obj, classes=classes)
+    
+    if classes is None: raise ValueError('CLASSES is required to be entered in order to calculate with the ROC version.')
+    ftns_name, metrics_module = 'ROC_OvO', check_and_import_library('model.plottable_metrics')
+    if ftns_name not in dir(metrics_module): raise ValueError(f'Warring: {ftns_name} is not in the model.metric library.')
+    use_ftns = getattr(metrics_module, ftns_name)
+    
     target_classes = list(np.unique(confusion_obj.actual_vector))
-    if list(target_classes) != list(confusion_obj.classes): 
-        if len(target_classes) == 1: # AUC area not available
-            if classes is None: return {c:0. for c in confusion_obj.classes}
-            return {c:0. for c in classes}
-        print(f"Warning: Classes in current label ({target_classes}) do not match classes in confusion object ({confusion_obj.classes}). Please ensure that the classes are consistent.")
-    crv = ROCCurve(actual_vector=confusion_obj.actual_vector, probs=confusion_obj.prob_vector, classes=target_classes)
-    return ftns._format_metric_for_multi_score(ftns_name, crv.area(), positive_class_indices)
+    if len(target_classes) == 1: return {c:0. for c in classes} # AUC area not available
+    if not all(class_item in list(target_classes) for class_item in list(classes)):
+        print(f"Warning: Classes in current label ({target_classes}) do not match classes in confusion object ({classes})."
+              +"Please ensure that the classes are consistent.")
+    
+    if confusion_obj.prob_vector is None: raise ValueError('No value for prob vector.')
+    if isinstance(confusion_obj.prob_vector, dict):
+        confusion_obj.prob_vector = confusion_obj.prob_vector['all_probs']
+        
+    roc_dict, _ = use_ftns(labels=confusion_obj.actual_vector, probs=confusion_obj.prob_vector, classes=classes, return_result=True)
+    return ftns._format_metric_for_multi_score('AUC', {t_c:auc for t_c, auc in zip(classes, roc_dict['auc'])}, positive_class_indices)
