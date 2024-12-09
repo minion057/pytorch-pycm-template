1460275149f1a54cc43464b4e6b5eda9cda0017f
minion057 <getit3981@gmail.com>
2024-12-09 14:45:36 +0900

    Fix: Add squeeze() to remove unnecessary dimensions

--- model/loss_functions/classification.py
+++ model/loss_functions/classification.py
@@ -11,7 +11,7 @@ def bce_loss(output, target, classes=None, device=None, **kwargs):
         target = convertOneHotEncoding(target, classes, useBinaryConversion=True, useMultiConversion=False)
         output = F.softmax(output, dim=1)[:, 1]  # Assuming class 1 is the positive class
         return F.binary_cross_entropy(output, target.float(), **kwargs)
-    return F.binary_cross_entropy_with_logits(output.squeeze(), target.to(device, dtype=torch.double), **kwargs)
+    return F.binary_cross_entropy_with_logits(output.squeeze(), target.to(device, dtype=torch.double).squeeze(), **kwargs)
 
 def focal_loss(output, target, classes=None, device=None, **kwargs):
     reduction = kwargs.get('reduction', 'mean')
@@ -20,7 +20,7 @@ def focal_loss(output, target, classes=None, device=None, **kwargs):
     if reduction not in ['mean', 'sum']: raise ValueError(f"Invalid reduction mode: {reduction}")
     if gamma is None: raise ValueError("Focal Loss requires 'gamma' to be set.")
     
-    ce_loss = F.cross_entropy(output, target, reduction='none')
+    ce_loss = F.cross_entropy(output.squeeze(), target.squeeze(), reduction='none')
     pt = torch.exp(-ce_loss)
     focal_loss = ((1-pt)**gamma * ce_loss)
     if alpha is not None:
@@ -39,7 +39,7 @@ def binary_focal_loss(output, target, classes=None, device=None, **kwargs): # on
         elif output.shape[-1] != 2: 
             raise ValueError('SigmoidFocalLoss is designed for binary classification tasks only. Multi-class classification is not supported.')
         else: output = output[:, 1] - output[:, 0]
-    return sigmoid_focal_loss(output.squeeze(), target.to(dtype=torch.float, device=device), **kwargs)
+    return sigmoid_focal_loss(output.squeeze(), target.to(dtype=torch.float, device=device).squeeze(), **kwargs)
 
 def soft_margin_loss(output, target, classes=None, device=None, **kwargs):
     if output.shape != target.shape and (len(output.shape) != 1 and len(target.shape) == 1):
@@ -48,7 +48,7 @@ def soft_margin_loss(output, target, classes=None, device=None, **kwargs):
         elif output.shape[-1] != 2: 
             raise ValueError('SoftMarginLoss is designed for binary classification tasks only. Multi-class classification is not supported.')
         else: output = output[:, 1] - output[:, 0]
-    return F.soft_margin_loss(output.squeeze(), target.to(dtype=torch.float, device=device), **kwargs)
+    return F.soft_margin_loss(output.squeeze(), target.to(dtype=torch.float, device=device).squeeze(), **kwargs)
 
 def hinge_embedding_loss(output, target, classes=None, device=None, **kwargs):
     if output.shape != target.shape and (len(output.shape) != 1 and len(target.shape) == 1):
@@ -57,7 +57,7 @@ def hinge_embedding_loss(output, target, classes=None, device=None, **kwargs):
         elif output.shape[-1] != 2: 
             raise ValueError('HingeEmbeddingLoss is designed for binary classification tasks only. Multi-class classification is not supported.')
         else: output = output[:, 1] - output[:, 0]
-    return F.hinge_embedding_loss(output.squeeze(), target.to(dtype=torch.float, device=device), **kwargs)
+    return F.hinge_embedding_loss(output.squeeze(), target.to(dtype=torch.float, device=device).squeeze(), **kwargs)
 
 def binary_auc_marging_loss(output, target, classes=None, device=None, **kwargs):
     # Large-scale Robust Deep AUC Maximization: A New Surrogate Loss and Empirical Studies on Medical Image Classification (2021, ICCV)
@@ -68,7 +68,7 @@ def binary_auc_marging_loss(output, target, classes=None, device=None, **kwargs)
     version = kwargs.get('version', 'v1')
     target = convertOneHotEncoding(target, classes, useBinaryConversion=True, useMultiConversion=False)
     loss_fn = losses.AUCMLoss(margin=margin, version=version, device='cpu' if device is None else device)
-    return loss_fn(output, target)
+    return loss_fn(output.squeeze(), target.squeeze())
 def auc_marging_loss(output, target, classes=None, device=None, **kwargs):
     # Large-scale Robust Deep AUC Maximization: A New Surrogate Loss and Empirical Studies on Medical Image Classification (2021, ICCV)
     # AUC-Margin loss with squared-hinge surrogate loss for optimizing AUROC
@@ -76,4 +76,4 @@ def auc_marging_loss(output, target, classes=None, device=None, **kwargs):
     # It is recommended to train first with CE LOSS and then use it for secondary training with BEST model.
     margin = kwargs.get('margin', 1.0)
     loss_fn = losses.MultiLabelAUCMLoss(margin=margin, num_labels=len(classes), device='cpu' if device is None else device)
-    return loss_fn(output, target)
+    return loss_fn(output.squeeze(), target.squeeze())
