d37af55228f124aea11003135868644c3333b2b3
minion057 <getit3981@gmail.com>
2025-01-14 16:11:57 +0900

    Fix: Correct typo

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -1,4 +1,4 @@
 from .mnist_data_loaders import MnistDataLoader
 from .imagenet1k_data_loaders import ImageNet1kDataLoader
 from .npz_loaders import NPZDataLoader
-from .excel_img_data_loaders import ExcelImgDataLoader
+from .excel_img_loaders import ExcelImgDataLoader
