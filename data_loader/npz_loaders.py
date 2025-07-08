22b61544203101578cca3fdd3dde160085435d81
minion057 <getit3981@gmail.com>
2025-07-08 13:39:22 +0900

    Feat: Add class removal option to npz loader

--- data_loader/npz_loaders.py
+++ data_loader/npz_loaders.py
@@ -2,13 +2,14 @@ import torch
 import numpy as np
 from torchvision import transforms
 from base import BaseSplitDataset, BaseSplitDatasetLoader
+from copy import deepcopy
 import data_loader.data_sampling as module_sampling
 
 class NPZDataset(BaseSplitDataset):
     """
     npz data loading demo using BaseSplitDataLoader
     """
-    def __init__(self, dataset_path:str, mode:str, trsfm=None):
+    def __init__(self, dataset_path:str, mode:str, trsfm=None, del_classes:list=None):
         if trsfm is None:
             trsfm = transforms.Compose([
                 transforms.ToTensor(),
@@ -17,6 +18,7 @@ class NPZDataset(BaseSplitDataset):
             ])
         super().__init__(dataset_path, mode, trsfm)
         self.data, self.targets, self.classes, self.paths, self.paths_per_class = self._load_data_list(self.init_kwargs['dataset_path'])
+        self._del_info_using_classes(del_classes)
     
     def __getitem__(self, index):
         """
@@ -52,11 +54,70 @@ class NPZDataset(BaseSplitDataset):
             else: print('Warning: No data path information available.')
         targets = torch.from_numpy(targets)
         return data, targets, classes, paths, paths_per_class
+    
+
+
+    def _del_info_using_classes(self, del_classes):
+        paths_per_class_copy = deepcopy(self.paths_per_class)
+        print(f'Original classes information: {self.paths_per_class.keys()}')
+        
+        keys_to_delete, paths_to_remove = [], []
+        for key, paths_in_key in paths_per_class_copy.items():
+            if any(del_class in key for del_class in del_classes):
+                keys_to_delete.append(key)
+                paths_to_remove.extend(paths_in_key)
+                print(f'Found del key: {key} (items: {len(paths_in_key)})')
+        print(f'Keys to be deleted: {keys_to_delete}')
+        print(f'Number of items to delete: {len(paths_to_remove)}')
+        
+        paths_to_remove_set = set(paths_to_remove)
+        current_paths = self.paths.tolist()
+        indices_to_delete = [
+            i for i, path in enumerate(current_paths)
+            if path in paths_to_remove_set
+        ]
+        indices_to_delete.sort(reverse=True)
+        print(f'Indices to be deleted in data: {len(indices_to_delete)}')
+
+        mask = np.ones(len(self.data), dtype=bool)
+        mask[indices_to_delete] = False
+        print(f'Number of items to delete: {len(indices_to_delete)}')
+        del_x = self.data[mask]
+        print(f'Results of deletion from X:     {len(self.data)} > {len(del_x)}')
+        del_y = self.targets[mask]
+        print(f'Results of deletion from y:     {len(self.targets)} > {len(del_y)}')
+        del_paths = self.paths[mask]
+        print(f'Results of deletion from paths: {len(self.paths)} > {len(del_paths)}')
+        
+        found_lingering_paths = False
+        for key, paths_in_key in self.paths_per_class.items():
+            if key in keys_to_delete: 
+                del paths_per_class_copy[key] # Delete the key from the dictionary
+                continue
+            # Check which of these paths are also in the paths_to_remove_set
+            still_matching_paths = [path for path in paths_in_key if path in paths_to_remove_set]
+            
+            if still_matching_paths:
+                found_lingering_paths = True
+                print(f'[WARNING] Key "{key}" in "paths_per_class" still contains paths that were targeted for deletion:') 
+                print(f'  Number of matching paths: {len(still_matching_paths)}') 
+                print(f'  Matching paths: {still_matching_paths}') 
+                print(f'  This information will be removed from "paths_per_class" only, as the actual data/targets/paths should already be deleted.')
+                
+                # Remove the matching paths from the current paths_in_key list
+                # Update paths_per_class_copy directly
+                paths_per_class_copy[key] = [path for path in paths_in_key if path not in paths_to_remove_set]
+                print(f'  Updated key "{key}" in "paths_per_class": {len(paths_in_key)} -> {len(paths_per_class_copy[key])}')
+        if not found_lingering_paths:
+            print("No lingering paths found in 'paths_per_class' related to the deleted classes. (Expected behavior)") 
+            
+        self.data, self.targets, self.paths, self.paths_per_class = del_x, del_y, del_paths, paths_per_class_copy
+        print(f'Change classes information: {self.paths_per_class.keys()}')
 
 class NPZDataLoader(BaseSplitDatasetLoader):
-    def __init__(self, dataset_path:str, mode:str, trsfm=None,
+    def __init__(self, dataset_path:str, mode:str, trsfm=None, del_classes:list=None,
                  batch_size:int=32, shuffle:bool=False, num_workers=0, collate_fn=None, **kwargs):       
-        super().__init__(dataset=NPZDataset(dataset_path, mode, trsfm), mode=mode, 
+        super().__init__(dataset=NPZDataset(dataset_path, mode, trsfm, del_classes), mode=mode, 
                          batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn, **kwargs)
         
     def _cal_mean_std(self):
